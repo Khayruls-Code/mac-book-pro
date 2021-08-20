2a200181e0ceb5515cfb5d77af22c3d2b2e9bbdd
Khayrul Islam <khayrulislam2525@gmail.com>
2021-08-21 02:15:33 +0600

    error solved

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const bestPrice = document.getElementById('bestPrice')
-const totalPrice = document.getElementById('totalPrice')
-const mainPrice = document.getElementById('mainPrice')
+const bestPrice = document.getElementById('bestPrice');
+const totalPrice = document.getElementById('totalPrice');
+const mainPrice = document.getElementById('mainPrice');
 
 //all price function
 function perticularPrice(priceId, price){
@@ -11,12 +11,12 @@ function perticularPrice(priceId, price){
 
 //total price function
 function totalPriceCount(){
-    return getId('bestPrice') + getId('memoryPrice') + getId('storagePrice') + getId('deliveryCost')   ///sum of all cost
+    return getId('bestPrice') + getId('memoryPrice') + getId('storagePrice') + getId('deliveryCost');  ///sum of all cost
 }
 
 //get single price function
 function getId(id){
-    let priceId = document.getElementById(id)
+    let priceId = document.getElementById(id);
     return parseFloat(priceId.innerText);
 }
 
@@ -24,25 +24,25 @@ function getId(id){
 //click event added here for all the particular button
 function eventAction(btnId, priceId, price){
     document.getElementById(btnId).addEventListener('click', function(){
-        perticularPrice(priceId, price)
-        totalPrice.innerText = totalPriceCount()
-        mainPrice.innerText = totalPriceCount()
+        perticularPrice(priceId, price);
+        totalPrice.innerText = totalPriceCount();
+        mainPrice.innerText = totalPriceCount();
     })
 }
 
-eventAction('memory8GB', 'memoryPrice', 0)
+eventAction('memory8GB', 'memoryPrice', 0);
 
-eventAction('memory16GB', 'memoryPrice', 180)
+eventAction('memory16GB', 'memoryPrice', 180);
 
-eventAction('SSD256GB', 'storagePrice', 0)
+eventAction('SSD256GB', 'storagePrice', 0);
 
-eventAction('SSD512GB', 'storagePrice', 100)
+eventAction('SSD512GB', 'storagePrice', 100);
 
-eventAction('SSD1TB', 'storagePrice', 180)
+eventAction('SSD1TB', 'storagePrice', 180);
 
-eventAction('costFree', 'deliveryCost', 0)
+eventAction('costFree', 'deliveryCost', 0);
 
-eventAction('withCharge', 'deliveryCost', 20)
+eventAction('withCharge', 'deliveryCost', 20);
 
 
 //promo code functionality
@@ -51,12 +51,12 @@ document.getElementById('applyBtn').addEventListener('click', function(){
     const codeInput = document.getElementById('codeInput');
     const codeInputValue = codeInput.value;
     if(codeInputValue == 'stevekaku'){
-        let percent = getId('mainPrice') * 20/100
-        let afterPercent = getId('mainPrice') - percent
+        let percent = getId('mainPrice') * 20/100;
+        let afterPercent = getId('mainPrice') - percent;
         mainPrice.innerText = afterPercent;
-        codeInput.value = ''
-        codeInput.setAttribute('disabled', true)
+        codeInput.value = '';
+        codeInput.setAttribute('disabled', true);
     }else{
-        alert('Invailed')
+        alert('Invailed');
     }
 })
